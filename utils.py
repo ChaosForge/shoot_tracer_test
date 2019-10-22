19ae856c88df8b0218616705b47eda498d3ba060
graph-zero <olgierdh@gmail.com>
2019-10-22 19:40:55 +0200

    Optimizations

--- utils.py
+++ utils.py
@@ -4,40 +4,26 @@ import pprint
 import math
 import re
 
-def mix_colors(src, dst):
-    sr,sg,sb,sa = src
-    dr,dg,db,da = dst 
-
-    sa_01 = sa / 255.0
-    da_01 = da / 255.0
-
-    r = sa_01 * sr + ( 1.0 - sa_01 ) * dr 
-    g = sa_01 * sg + ( 1.0 - sa_01 ) * dg 
-    b = sa_01 * sb + ( 1.0 - sa_01 ) * db 
-    a = sa_01 * 255.0
-
-    return (r,g,b,a) 
-
-def extract_color(p, x):
-    return (p[x], p[x + 1], p[x + 2], p[x + 3])
-
 class picture(object):
     def __init__(self, width, height, pixel_size = 1):
         self.__array = numpy.full((height,width*4), 255, dtype=numpy.uint8)
+        self.__view = self.__array.view().reshape(-1,4)
         self.__widht = width
         self.__heihgt = height
         self.__pixel_size = pixel_size
 
     def put_pixel(self, x, y, color):
         row = y * self.__pixel_size 
-        col = x * 4 * self.__pixel_size 
-        for y in range(row, row + self.__pixel_size):
-            for x in range(col, col + self.__pixel_size * 4, 4):
-                r,g,b,a = mix_colors(color, extract_color(self.__array[y],x))
-                self.__array[y][x+0] = r
-                self.__array[y][x+1] = g
-                self.__array[y][x+2] = b
-                self.__array[y][x+3] = a
+        col = x * self.__pixel_size
+        c = numpy.array(color)
+        sa_01 = c[3] / 255.0
+        o_m_a = 1.0 - sa_01
+        sc = c * sa_01 
+        for yy in range(row, row + self.__pixel_size):
+            index = self.__widht * yy 
+            for xx in range(col, col + self.__pixel_size):
+                idx = index + xx
+                self.__view[idx] = sc + o_m_a * self.__view[idx]
 
     def save(self, filename):
         png.from_array(self.__array, mode='RGBA').save(filename)
