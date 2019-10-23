0ffdb3f96ac8673f9d9a7514daf4764dad179e16
graph-zero <olgierdh@gmail.com>
2019-10-23 15:42:30 +0200

    Added path colors

--- test.py
+++ test.py
@@ -9,9 +9,13 @@ def run_case(map_file_name, output_file_name):
     m,player_pos,enemy_pos,expected_result = utils.load_map(map_file_name)
     utils.draw_map(m,p)
     r,succ = trace_shoot(m,player_pos,enemy_pos)
-    utils.draw_route(r,p)
+    r1,succ1 = trace_shoot(m,enemy_pos,player_pos)
+
+    utils.draw_route(r,p,(64,220,0,128))
+    utils.draw_route(r1,p,(220,64,0,128))
+
     p.save(output_file_name)
-    if succ == expected_result:
+    if succ == expected_result and succ1 == expected_result:
         print("{} [OK]".format(map_file_name))
     else: 
         print("{} [FAILED]".format(map_file_name))
