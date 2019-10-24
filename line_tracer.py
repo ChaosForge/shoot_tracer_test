d3451b530797e19c58a83d5527d84493966f244a
graph-zero <olgierdh@gmail.com>
2019-10-24 14:43:00 +0200

    Use new line tracing algorithm instead of bresenham

--- line_tracer.py
+++ line_tracer.py
@@ -11,19 +11,26 @@ def calc_dds(x0, y0, x1, y1):
 
     step = adx if adx >= ady else ady
     
+    if step == 0:
+        return (1, 1, int(step))
+
     dx = dx / step
     dy = dy / step
 
     return (dx, dy, int(step))
 
-def plot_line(x0, y0, x1, y1):
+def trace_line(m, x0, y0, x1, y1):
     ret = []
 
     dx, dy, step = calc_dds(x0, y0, x1, y1)
 
     for i in range(0, step + 1):
-        x = x0 + i * dx
-        y = y0 + i * dy
-        ret.append((round(x), round(y)))
+        x = round( x0 + i * dx )
+        y = round( y0 + i * dy )
 
-    return ret
+        if m[y][x]:
+            return (ret, False)
+
+        ret.append((x,y))
+
+    return (ret, True)
